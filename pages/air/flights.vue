2834761ec484507c35e59befcdc75433c9eca762
zengWeiJia <1056548949@qq.com>
2019-08-04 14:30:14 +0800

    机票列表渲染完成，分页完成

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,72 +1,114 @@
 <template>
-    <section class="contianer">
-        <el-row  type="flex" justify="space-between">
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div />
 
-            <!-- 顶部过滤列表 -->
-            <div class="flights-content">
-                <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
-                
-                <!-- 航班头部布局 -->
-                <div>
-                    <FlightsListHead></FlightsListHead>
-                </div>
-                
-                
-                <!-- 航班信息 -->
-                <div>
-                    <FlightsItem
+        <!-- 航班头部布局 -->
+        <div>
+          <FlightsListHead />
+        </div>
 
-                    ></FlightsItem>
-                </div>
-            </div>
+        <!-- 航班信息 -->
+        <div>
+          <!-- 航班列表 -->
+          <flightsItem v-for="(item, index) in dataList" :key="index" :data="item" />
+          <!-- 分页 -->
+          <el-row type="flex" justify="center" style="margin-top:10px;">
+            <!-- size-change：切换条数时候触发 -->
+            <!-- current-change：选择页数时候触发 -->
+            <!-- current-page: 当前页数 -->
+            <!-- page-size：当前条数 -->
+            <!-- total：总条数 -->
+            <el-pagination
+              :current-page="pageIndex"
+              :page-sizes="[5, 10, 15, 20]"
+              :page-size="pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="flightsData.total"
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+            />
+          </el-row>
+        </div>
+      </div>
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
-        </el-row>
-    </section>
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
-
-
 import FlightsListHead from '@/components/air/flightsListHead.vue'
 import FlightsItem from '@/components/air/flightsItem.vue'
 export default {
-    components: {
-        FlightsListHead,
-        FlightsItem
-    },
-    data(){
-        return {
-            
-        }
-    },
-    methods: {
-        
-    },
-    mounted () {
-        console.log(this.$route.query)
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  data() {
+    return {
+      flightsData: {}, // 航班总数据
+      dataList: [], // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+      pageIndex: 1, // 当前页数
+      pageSize: 5 // 显示条数
     }
+  },
+
+  mounted() {
+    this.getData()
+  },
+
+  methods: {
+    // 设置dataList数据
+    setDataList() {
+      const start = (this.pageIndex - 1) * this.pageSize
+      const end = start + this.pageSize
+      this.dataList = this.flightsData.flights.slice(start, end)
+    },
+    // 获取航班总数据
+    getData() {
+      this.$axios({
+        url: `airs`,
+        params: this.$route.query // 来自URL的5个参数
+      }).then((res) => {
+        this.flightsData = res.data
+        this.setDataList()
+      })
+    },
+    // 切换条数时触发
+    handleSizeChange(value) {
+      this.pageSize = value
+      this.pageIndex = 1
+      this.setDataList()
+    },
+
+    // 切换页数时触发
+    handleCurrentChange(value) {
+      this.pageIndex = value
+      this.setDataList()
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
-    .contianer{
-        width:1000px;
-        margin:20px auto;
-    }
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
 
-    .flights-content{
-        width:745px;
-        font-size:14px;
-    }
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
 
-    .aside{
-        width:240px;
-    } 
+.aside {
+  width: 240px;
+}
 </style>
